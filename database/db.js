891db82fafaee21c110c2adab4f0dfc95a1c2e24
Rithk <rithkchaudhary86@gmail.com>
2024-01-04 01:16:24 +0530

    initial commit

---
+++ database/db.js
@@ -0,0 +1,27 @@
+const mongoose= require('mongoose');
+mongoose.connect("mongodb+srv://naruto:jVSNMMGwfSVPzHXV@cluster0.78poq1d.mongodb.net/Bank")
+
+const AdminSchema= new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+const UserSchema= new mongoose.Schema({
+    Name: String,
+    PhoneNumber: String,
+    username: String,
+    password: String,
+    tokenId: String,
+    email: String,
+    BankBalance: String,
+    
+
+})
+
+const Admin=mongoose.model('Admin',AdminSchema);
+const User =mongoose.model('User',UserSchema);
+
+module.exports={
+    Admin,
+    User
+}
